08c854b42bb3d80e4675470e6dbf990555237c24
kchapelier <kevin.chapelier@gmail.com>
2017-01-16 13:28:34 +0100

    first commit (mostly working except for resizes and quality)

---
+++ README.md
@@ -0,0 +1,5 @@
+# procjam2014-regl
+
+Partial remake of my [procjam2014](https://github.com/kchapelier/procjam2014/) project with [regl](http://regl.party/).
+
+Mostly for learning and evaluating regl.
---
+++ src/index.js
@@ -0,0 +1,157 @@
+"use strict";
+
+var createREGL = require('regl'),
+    touch = require('touches');
+
+function init() {
+    var canvas = document.getElementById('canvas'),
+        fragmentShaderSource = document.getElementById('fragmentshader').innerHTML,
+        vertexShaderSource = document.getElementById('vertexshader').innerHTML;
+
+    var regl = createREGL(canvas);
+
+    var needUpdate = true;
+
+    var options = {
+        quality: 1.,
+        actualQuality: 1.,
+        scale: 1.,
+        distortion: 1.,
+        seaLevel: 0.45,
+        moveX: 0,
+        moveY : 0,
+        debug: ''
+    };
+
+    /* EVENTS */
+
+    var down = false,
+        downPosition = [0,0],
+        currentPosition = [0,0];
+
+    var touchEvents = touch(window, { target: canvas, filtered: true });
+
+    touchEvents.on('start', function (e, position) {
+        if (!e.ctrlKey && !e.metaKey) {
+            if (!down) {
+                down = true;
+                downPosition[0] = currentPosition[0] = position[0];
+                downPosition[1] = currentPosition[1] = position[1];
+            }
+        }
+    });
+
+    touchEvents.on('move', function (e, position) {
+        if (down) {
+            currentPosition[0] = position[0];
+            currentPosition[1] = position[1];
+            needUpdate = true;
+        }
+    });
+
+    touchEvents.on('end', function (e, position) {
+        down = false;
+        needUpdate = true;
+        downPosition[0] = downPosition[1] = currentPosition[0] = currentPosition[1] = 0;
+    });
+
+    window.addEventListener('resize', function () { needUpdate = true; }, false);
+    window.addEventListener('wheel', function (e) {
+        e.preventDefault();
+        options.scale = Math.max(0.25, Math.min(5, options.scale + e.deltaY / 200));
+        needUpdate = true;
+    }, false);
+
+    /* RENDERING */
+
+    function resize(options) {
+        var quality = options.actualQuality;
+        quality = 1;
+        canvas.style.webkitTransform = canvas.style.transform = 'scale3d(' + quality + ', ' + quality + ', 1)';
+        canvas.width = Math.ceil(document.body.clientWidth / quality);
+        canvas.height = Math.ceil(document.body.clientHeight / quality);
+    }
+
+    var drawTriangle = regl({
+        frag: fragmentShaderSource,
+        vert: vertexShaderSource,
+        attributes: {
+            position: regl.buffer([
+                [-2, -2],   // no need to flatten nested arrays, regl automatically
+                [4, -2],    // unrolls them into a typedarray (default Float32)
+                [0,  4]
+            ])
+        },
+        uniforms: {
+            iMove: regl.prop('move'),
+            iScale: regl.prop('scale'),
+            iDistortion: regl.prop('distortion'),
+            iSeaLevel: regl.prop('seaLevel')
+        },
+        count: 3
+    });
+
+    function drawScene (options) {
+        // clear contents of the drawing buffer
+        regl.clear({
+            scale: options.scale,
+            distortion: options.distortion,
+            seaLevel: options.seaLevel,
+            move: [options.moveX, options.moveY],
+            depth: 1
+        });
+
+        // draw a triangle using the command defined above
+        drawTriangle({
+            scale: options.scale,
+            distortion: options.distortion,
+            seaLevel: options.seaLevel,
+            move: [options.moveX, options.moveY]
+        })
+    }
+
+    var previousTime = 0;
+
+    regl.frame(function (frameOptions) {
+        var time = frameOptions.time * 1000;
+
+        var deltaTime = time - previousTime;
+
+        if (needUpdate) {
+            options.moveX += (currentPosition[0] - downPosition[0]) / 60 * deltaTime;
+            options.moveY += (downPosition[1] - currentPosition[1]) / 60 * deltaTime;
+            options.actualQuality = down ? Math.max(3, options.quality) : options.quality;
+
+            resize(options);
+            drawScene(options);
+            needUpdate = !!down;
+        }
+
+        previousTime = time;
+    });
+
+
+    var gui = new dat.GUI(),
+        controllers = [];
+
+    controllers.push(gui.add(options, 'quality', { High: 0.5, Good: 1, Medium: 2, Bad: 5 }));
+    controllers.push(gui.add(options, 'scale').min(0.25).max(5).step(0.01).listen());
+    controllers.push(gui.add(options, 'distortion').min(0.).max(3).step(0.01));
+    controllers.push(gui.add(options, 'seaLevel').min(0.3).max(0.6).step(0.01));
+    controllers.push(gui.add(options, 'moveX').listen());
+    controllers.push(gui.add(options, 'moveY').listen());
+
+    controllers.forEach(function (controller) {
+        controller.onChange(function () {
+            needUpdate = true;
+        });
+
+        controller.onFinishChange(function () {
+            needUpdate = true;
+        });
+    });
+}
+
+
+
+init();
